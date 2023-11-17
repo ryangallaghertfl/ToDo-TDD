9e2b428f36aab42b395cfacefc7432a7100234b6
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-17 14:52:56 +0000

    test: added LocationTests file will import CoreLocation since it will use for map coordinates

---
+++ ToDo-TDDTests/LocationTests.swift
@@ -0,0 +1,22 @@
+//
+//  LocationTests.swift
+//  ToDo-TDDTests
+//
+//  Created by Ryan Gallagher on 17/11/2023.
+//
+
+import XCTest
+@testable import ToDo_TDD
+import CoreLocation
+
+final class LocationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
