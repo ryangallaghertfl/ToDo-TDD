938dd299d1a582ab8f63431150e46e7dcfc5790a
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-17 14:32:47 +0000

    test: changed unit test names to _test_targetName_methodName_description format

--- ToDo-TDDTests/ToDoItemTests.swift
+++ ToDo-TDDTests/ToDoItemTests.swift
@@ -18,12 +18,12 @@ final class ToDoItemTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_init_whenGivenTitle_setsTitle() {
+    func test_ToDoItem_init_whenGivenTitle_setsTitle() {
         let item = ToDoItem(title: "Dummy")
         XCTAssertEqual(item.title, "Dummy")
     }
     
-    func test_init_whenGivenDescription_setsDescription() {
+    func test_ToDoItem_init_whenGivenDescription_setsDescription() {
         let item = ToDoItem(title: "Dummy", itemDescription: "Dummy Description")
         XCTAssertEqual(item.itemDescription, "Dummy Description")
     }
