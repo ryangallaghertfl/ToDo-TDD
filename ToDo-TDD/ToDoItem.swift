8457ad881c7199f592520760509fdcbfa5ac2e53
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-17 14:18:18 +0000

    test: added test file, first unit test for ToDoItem struct

---
+++ ToDo-TDD/ToDoItem.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoItem.swift
+//  ToDo-TDD
+//
+//  Created by Ryan Gallagher on 17/11/2023.
+//
+
+import Foundation
+
+struct ToDoItem {
+    let title: String
+}
---
+++ ToDo-TDDTests/ToDoItemTests.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoItemTests.swift
+//  ToDo-TDDTests
+//
+//  Created by Ryan Gallagher on 17/11/2023.
+//
+
+import XCTest
+@testable import ToDo_TDD
+
+final class ToDoItemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_init_takesTitle() {
+        let item = ToDoItem(title: "Dummy")
+    }
+
+
+}
