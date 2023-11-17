71a324268ef7f5e2ff69bbeeb3b38354115b001c
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-17 15:04:33 +0000

    test: Location init, should init coordinate property correctly, also added to Location struct

--- ToDo-TDDTests/LocationTests.swift
+++ ToDo-TDDTests/LocationTests.swift
@@ -18,5 +18,14 @@ final class LocationTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func test_Location_init_setsCoordinate() throws {
+        let coordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        
+        let location = Location(name: "", coordinate: coordinate)
+        let resultCoordinate = try XCTUnwrap(location.coordinate)
+        XCTAssertEqual(resultCoordinate.latitude, 1, accuracy: 0.000_001)
+        XCTAssertEqual(resultCoordinate.longitude, 2, accuracy: 0.000_001)
+    }
 
 }
