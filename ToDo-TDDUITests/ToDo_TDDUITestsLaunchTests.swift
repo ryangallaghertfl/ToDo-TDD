d2d4f2999412a7907e92feb45f538c405dfa3ba5
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-17 11:53:09 +0000

    Initial Commit

---
+++ ToDo-TDDUITests/ToDo_TDDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDo_TDDUITestsLaunchTests.swift
+//  ToDo-TDDUITests
+//
+//  Created by Ryan Gallagher on 17/11/2023.
+//
+
+import XCTest
+
+final class ToDo_TDDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
