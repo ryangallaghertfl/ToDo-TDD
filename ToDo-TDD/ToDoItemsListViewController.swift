63ae48817fdfbfdbe6667022ce71553c1a8f63ea
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-19 16:53:50 +0000

    feat: make ToDoItemsListViewC datasource of tableView, make VC conform to UITableViewDataSource

--- ToDo-TDD/ToDoItemsListViewController.swift
+++ ToDo-TDD/ToDoItemsListViewController.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class ToDoItemsListViewController: UIViewController {
-   
+    
     @IBOutlet var tableView: UITableView!
+    
     var toDoItemStore: ToDoItemStoreProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+    }
+    
+}
+
+//MARK: extending to conform to UITableViewDataSource
+
+extension ToDoItemsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 1}
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return UITableViewCell()
+    }
 }
