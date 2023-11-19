8e6e2280d177a0f76d5e9a895ba99e633873390b
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-19 17:03:20 +0000

    feat: ToDoItemsListVC now returns correct number of items, successfully subs to publisher

--- ToDo-TDD/ToDoItemsListViewController.swift
+++ ToDo-TDD/ToDoItemsListViewController.swift
@@ -6,16 +6,25 @@
 //
 
 import UIKit
+import Combine
 
 class ToDoItemsListViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
-    
     var toDoItemStore: ToDoItemStoreProtocol?
+    private var items: [ToDoItem] = [] //holds items sent by item publisher
+    private var token: AnyCancellable? //holds ref to subscriber subscribed by the publisher
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        
+        
+        //subscribe to changes sent by the item publisher of toDoItemStore
+        token = toDoItemStore?.itemPublisher
+        .sink(receiveValue: { [weak self] items in
+        self?.items = items //store items in our array
+        })
     }
     
 }
@@ -24,7 +33,9 @@ class ToDoItemsListViewController: UIViewController {
 
 extension ToDoItemsListViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return 1}
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
