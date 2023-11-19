90733dc6fc0f12826d1f0fb1a81df4e353911fad
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-19 14:55:42 +0000

    test: ToDoItemStore, added test for when user checks ToDoItem as done, list of items written to file system

--- ToDo-TDDTests/ToDoItemStoreTests.swift
+++ ToDo-TDDTests/ToDoItemStoreTests.swift
@@ -64,6 +64,34 @@ final class ToDoItemStoreTests: XCTestCase {
         token.cancel()
         XCTAssertEqual(result, [toDoItem])
     }
+    
+    func test_ToDoItemStore_init_whenItemIsChecked_shouldLoadPreviousToDoItems_AssertsEqual() throws {
+        
+        var sut1: ToDoItemStore? = ToDoItemStore(fileName: "dummy_store")
+        
+        let publisherExpectation = expectation(description: "Wait for publisher in \(#file)")
+        
+        let toDoItem = ToDoItem(title: "Dummy Title")
+        
+        sut1?.add(toDoItem)
+        sut1?.check(toDoItem)
+        sut1 = nil
+        
+        let sut2 = ToDoItemStore(fileName: "dummy_store")
+        
+        var result: [ToDoItem]?
+        
+        let token = sut2.itemPublisher
+            .sink { value in
+                result = value
+                publisherExpectation.fulfill()
+            }
+        
+        wait(for: [publisherExpectation], timeout: 1)
+        token.cancel()
+        
+        XCTAssertEqual(result?.first?.done, true)
+        }
 
 }
 
