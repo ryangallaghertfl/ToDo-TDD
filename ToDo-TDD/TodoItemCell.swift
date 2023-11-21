54bc1e97607291c219d585769ea9607693a351ec
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-21 12:38:16 +0000

    refactor: added LocationLabel to ToDoItemCell and added to subview of contentview, test now passes

--- ToDo-TDD/TodoItemCell.swift
+++ ToDo-TDD/TodoItemCell.swift
@@ -11,16 +11,19 @@ class ToDoItemCell: UITableViewCell {
     
     let titleLabel: UILabel
     let dateLabel: UILabel
+    let locationLabel: UILabel
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         titleLabel = UILabel()
         dateLabel = UILabel()
+        locationLabel = UILabel()
         
         super.init(style: style,
                    reuseIdentifier: reuseIdentifier)
         
         contentView.addSubview(titleLabel)
         contentView.addSubview(dateLabel)
+        contentView.addSubview(locationLabel)
     }
     
     required init?(coder: NSCoder) { fatalError() }
