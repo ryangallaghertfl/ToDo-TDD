7781527a7c5a33629b6b8cd381999f8915ce5f5b
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-21 12:32:11 +0000

    refactor: added dateLabel property to ToDoItemCell and added to contentView subview

--- ToDo-TDD/TodoItemCell.swift
+++ ToDo-TDD/TodoItemCell.swift
@@ -10,14 +10,17 @@ import UIKit
 class ToDoItemCell: UITableViewCell {
     
     let titleLabel: UILabel
+    let dateLabel: UILabel
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         titleLabel = UILabel()
+        dateLabel = UILabel()
         
         super.init(style: style,
                    reuseIdentifier: reuseIdentifier)
         
         contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
     }
     
     required init?(coder: NSCoder) { fatalError() }
