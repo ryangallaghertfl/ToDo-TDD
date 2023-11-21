d01d8ce0fa1f7fe5fb553d90c4805ba01e85b4e5
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-21 12:21:40 +0000

    refactor: changed ToDoItemCell so that titleLabel is now added to subView, test now passes

--- ToDo-TDD/TodoItemCell.swift
+++ ToDo-TDD/TodoItemCell.swift
@@ -9,4 +9,12 @@ import UIKit
 
 class ToDoItemCell: UITableViewCell {
     let titleLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style,
+                   reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titleLabel)
+    }
+    
+    required init?(coder: NSCoder) { fatalError() }
 }
