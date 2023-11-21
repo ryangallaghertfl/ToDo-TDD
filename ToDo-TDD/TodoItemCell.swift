1f4c116bb67e0e40b90182c69505667d074ac42a
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-21 11:39:37 +0000

    UI: renamed ToDoITemTableViewCell to TodoItemCell, added UILabel property

---
+++ ToDo-TDD/TodoItemCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodoItemCell.swift
+//  ToDo-TDD
+//
+//  Created by Ryan Gallagher on 21/11/2023.
+//
+
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+    let titleLabel = UILabel()
+}
