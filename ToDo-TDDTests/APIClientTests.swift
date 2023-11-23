cae614e58b874f59aa036cf4abaea658be997ee6
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-23 16:01:40 +0000

    test: APIClient, added test to check that coordinate is fetched

--- ToDo-TDDTests/APIClientTests.swift
+++ ToDo-TDDTests/APIClientTests.swift
@@ -5,6 +5,9 @@
 //  Created by Ryan Gallagher on 23/11/2023.
 //
 import XCTest
+//Intents and Contacts useful for creating CLPlacemark instances in tests
+import Intents
+import Contacts
 @testable import ToDo_TDD
 
 final class APIClientTests: XCTestCase {
@@ -17,6 +20,27 @@ final class APIClientTests: XCTestCase {
 
     override func tearDownWithError() throws {
         sut = nil
+    }
+    
+    func test_APIClient_coordinate_fetchesCoordinate() {
+      let geoCoderMock = GeoCoderProtocolMock()
+      sut.geoCoder = geoCoderMock
+      let location = CLLocation(latitude: 1,
+                                longitude: 2)
+      let placemark = CLPlacemark(location: location,
+                                  name: nil,
+                                  postalAddress: nil)
+
+      var result: Coordinate?
+      sut.coordinate(for: "") { coordinate in
+        result = coordinate
+      }
+      geoCoderMock.completionHandler?([placemark], nil)
+
+      XCTAssertEqual(result?.latitude,
+                     location.coordinate.latitude)
+      XCTAssertEqual(result?.longitude,
+                     location.coordinate.longitude)
     }
 
    
