5083baee03807c8cc5e7aabc69e94a67c730738c
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-24 11:26:27 +0000

    test: ToDoItemLVC, changed setupwitherror to identify root VC via identifier

--- ToDo-TDDTests/ToDoItemsListViewControllerTests.swift
+++ ToDo-TDDTests/ToDoItemsListViewControllerTests.swift
@@ -12,14 +12,18 @@ final class ToDoItemsListViewControllerTests: XCTestCase {
     
     var sut: ToDoItemsListViewController!
     var toDoItemStoreMock: ToDoItemStoreProtocolMock!
-
+    
     override func setUpWithError() throws {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        sut = try XCTUnwrap(
-        storyboard.instantiateInitialViewController() as? ToDoItemsListViewController)
-        toDoItemStoreMock = ToDoItemStoreProtocolMock()
-        sut.toDoItemStore = toDoItemStoreMock
-        sut.loadViewIfNeeded()
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      sut = try 
+        XCTUnwrap(
+        storyboard.instantiateViewController(
+          withIdentifier: "ToDoItemsListViewController")
+        as? ToDoItemsListViewController
+      )
+      toDoItemStoreMock = ToDoItemStoreProtocolMock()
+      sut.toDoItemStore = toDoItemStoreMock
+      sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
