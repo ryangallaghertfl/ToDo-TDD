a6f07de2f089ca5448d7c2cd2831476564a06400
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-24 11:32:55 +0000

    refator: SceneDelegate calls AppCoordinator.start

--- ToDo-TDD/SceneDelegate.swift
+++ ToDo-TDD/SceneDelegate.swift
@@ -10,13 +10,25 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let _ = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        window = UIWindow(windowScene: scene as! UIWindowScene)
+        
+        let coordinator = AppCoordinator(window: window)
+        coordinator.start() //call the start
+        appCoordinator = coordinator
+        
+        window?.makeKeyAndVisible()
+        //tell UIKit that this window should be presented on the screen
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
