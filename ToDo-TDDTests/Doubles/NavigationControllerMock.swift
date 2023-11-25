866d509f980dc7afdf9a27a8e8ecc65ada0bc019
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-25 14:40:22 +0000

    test: NavigationControllerMock, method so we can track dismiss(animated:completion)

--- ToDo-TDDTests/Doubles/NavigationControllerMock.swift
+++ ToDo-TDDTests/Doubles/NavigationControllerMock.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class NavigationControllerMock: UINavigationController {
     var lastPushedViewController: UIViewController?
+    var dismissCallCount = 0
     
     override func pushViewController(_ viewController: UIViewController, animated: Bool) {
         lastPushedViewController = viewController
         super.pushViewController(viewController, animated: animated)
+    }
+    
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        dismissCallCount += 1
+        super.dismiss(animated: flag, completion: completion)
     }
 }
