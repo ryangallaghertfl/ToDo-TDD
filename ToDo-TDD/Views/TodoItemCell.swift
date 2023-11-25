2ee564fbea8beff90c2504e74e8a0086572da90c
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-25 14:34:56 +0000

    refactor: UI, added NSLayoutC for ToDoItemCell

--- ToDo-TDD/Views/TodoItemCell.swift
+++ ToDo-TDD/Views/TodoItemCell.swift
@@ -16,14 +16,27 @@ class ToDoItemCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         titleLabel = UILabel()
         dateLabel = UILabel()
+        dateLabel.textAlignment = .right
+        
         locationLabel = UILabel()
+        
+        let titleLocation = UIStackView(arrangedSubviews: [titleLabel, locationLabel])
+        titleLocation.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [titleLocation, dateLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
         super.init(style: style,
                    reuseIdentifier: reuseIdentifier)
         
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(dateLabel)
-        contentView.addSubview(locationLabel)
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+        ])
     }
     
     required init?(coder: NSCoder) { fatalError() }
